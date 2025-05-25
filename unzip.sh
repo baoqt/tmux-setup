bf1bf9c36971025fba66f1e4ed0c18e6c8e592bf
Bao <baoq.tran@outlook.com>
2025-05-25 16:32:49 -0700

    Removing renamed and deleted files

--- unzip.sh
+++
@@ -1,5 +0,0 @@
-# /usr/bin/bash
-unzip tmux.zip -d .
-rm tmux.zip
-unzip nvim.zip -d .
-rm nvim.zip
--- zip.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-zip -r tmux.zip tmux
-rm -rf tmux
-zip -r nvim.zip nvim
-rm -rf nvim
